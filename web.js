aa377a22439a2074106795901e95fffb64d60f27
hrguez <helena.rodriguez.gijon@gmail.com>
2013-07-25 18:23:37 +0000

    Modified web.js to read index

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    buffer = fs.write('index.html');
+    buffer = fs.readFileSync('index.html');
     response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-}); 
+});
